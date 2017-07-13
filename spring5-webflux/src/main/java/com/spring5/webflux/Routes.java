d0633518001f21c9d1afb47b8a20cf5f63f59894
宋运旗 <songyunqi@qianmi.com>
2017-07-13 16:52:43 +0800

    webflux

--- spring5-webflux/src/main/java/com/spring5/webflux/Routes.java
+++ spring5-webflux/src/main/java/com/spring5/webflux/Routes.java
@@ -10,9 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
-import org.springframework.web.reactive.function.server.RouterFunction;
 import static org.springframework.web.reactive.function.server.RouterFunctions.resources;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
