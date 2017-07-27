79b31fc103c5647bb327059a2898e855b49b08a6
宋运旗 <songyunqi@qianmi.com>
2017-07-27 14:05:47 +0800

    webflux

--- spring5-boot-webflux/src/main/java/com/spring5/boot/webflux/Routes.java
+++ spring5-boot-webflux/src/main/java/com/spring5/boot/webflux/Routes.java
@@ -8,13 +8,11 @@ package com.spring5.boot.webflux;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import org.springframework.http.MediaType;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
 /**
  *
