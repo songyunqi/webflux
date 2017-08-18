f15cd62aab346eb01c6462c58385f483eead8901
宋运旗 <songyunqi@qianmi.com>
2017-08-18 11:19:01 +0800

    webflux

--- spring5-base/src/main/java/com/spring5/base/rep/UserRep.java
+++ spring5-base/src/main/java/com/spring5/base/rep/UserRep.java
@@ -6,7 +6,7 @@
 package com.spring5.base.rep;
 
 import com.spring5.base.domain.User;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -14,6 +14,6 @@ import org.springframework.stereotype.Repository;
  * @author Yang
  */
 @Repository
-public interface UserRep extends ReactiveCrudRepository<User,Long> {
+public interface UserRep extends JpaRepository<User,Long> {
 
 }
