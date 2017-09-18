7a77fa91dc7d5ae0344b574342299fdbc503a76e
宋运旗 <songyunqi@qianmi.com>
2017-09-18 18:00:41 +0800

    webflux

---
+++ spring-oauth2/src/main/java/com/foo/config/AuthServerConfigAdapter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.foo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+/**
+ *
+ * @author Yang
+ */
+@Configuration
+public class AuthServerConfigAdapter extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory() // 使用in-memory存储
+                .withClient("client") // client_id
+                .secret("secret") // client_secret
+                .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
+                .scopes("app"); // 允许的授权范围
+    }
+}
