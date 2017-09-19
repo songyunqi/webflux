8cfb825a4c7c922796f82ab159afd67a8383e8fe
宋运旗 <songyunqi@qianmi.com>
2017-09-19 17:03:14 +0800

    webflux

--- spring-oauth2/src/main/java/com/foo/Application.java
+++ spring-oauth2/src/main/java/com/foo/Application.java
@@ -5,9 +5,13 @@
  */
 package com.foo;
 
+import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
@@ -15,10 +19,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @SpringBootApplication
 @EnableAuthorizationServer
+@RestController
+@EnableResourceServer
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
 }
